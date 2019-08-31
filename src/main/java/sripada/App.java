5de755b7bf6b795d956ce7767ae088a47b20a21e
sripada <sripada>
2019-08-31 12:49:59 +0530

    my mvn commits

---
+++ src/main/java/sripada/App.java
@@ -0,0 +1,13 @@
+package sripada;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
